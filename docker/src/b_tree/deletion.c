1db596a13daae8b2069312f4c3cc4e30665ef746
Adrien THIERRY <athierry@f2r9s9.clusters.42paris.fr>
2025-06-10 20:36:23 +0200

    courses

--- docker/src/b_tree/deletion.c
+++ docker/src/b_tree/deletion.c
@@ -6,7 +6,7 @@
 /*   By: athierry <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 17:20:46 by athierry          #+#    #+#             */
-/*   Updated: 2025/06/04 19:17:23 by athierry         ###   ########.fr       */
+/*   Updated: 2025/06/10 20:36:13 by athierry         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,46 @@ static int __bFindSibling(__bNode* node){
 }
 
 
-static void __bMergeNode(&place){
+static __bRes __bMergeNode(&place){
 
-}
-
-static void __bMergeLeaf(&place){
 }
 
 typedef void	(*__bRotateFunc)(__bRes, int);
+
+static void __bBalance(__bTree* tree, __bRes merged){
+	while (merged.node){
+		if ((*merged.node).empty){
+
+		}
+		else{
+			for (int i = merged.rank + 1; i < (*merged.node).count; i++){
+				(*merged.node).dividors[i - 1] = (*merged.node).dividors[i];
+				(*merged.node).children[i] = (*merged.node).dividors[i + 1]; // think again
+				(*merged.node).count -= 1;
+				(*merged.node).empty = (*merged.node).count == BVALUE / 2;
+			}
+
+		}
+	}
+}
+
+
+
+static __bRes __bMergeLeaf(__bRes place){
+	for (int i = place.rank; i < (*place.node).count; i++)
+		(*place.node).dividors[i] = (*place.node).dividors[i + 1];
+	(*place.node).count -= 1;
+	if ((*place.node).rank == (*(*place.node).parent).count - 1){
+		place.node = (*(*place.node).parent).children[(*place.node).rank - 1];
+	}
+	(*place.node).dividors[(*place.node).count++] = (*(*place.node).parent).dividors[(*place.node).rank];
+	__bNode* sibling = (*(*place.node).parent).children((*place.node).rank + 1);
+	for (int i = 0; i < (*sibling).count; i++){
+		(*place.node).dividors[(*place.node).count++] = (*sibling).dividors[i];
+	}
+	__bFree(sibling);
+}
+
 
 
 static void __bDeleteEmptyLeaf(__bRes place){
@@ -74,7 +106,7 @@ static void __bDeleteNonEmptyLeaf(__bRes place){
    place = __bMergeNode(place);
    return place;
    }
-*/
+   */
 
 
 
@@ -84,42 +116,42 @@ static  void __bDeleteLeaf(__bRes place){
 }
 
 __bRes __bMaxMin(__bRes place){
-    __bNode* actual = (*place.node).children[place.rank];
-    __bRes res = {NULL, 0};
-    int steps = 0;
-    while ((*actual).children[0]){
-        steps++;
-        actual =(*actual).children[(*actual).count];
-        if (!(*actual).empty)
-            res.rank = steps;
-    }
-    res.node = actual;
-    return res;
+	__bNode* actual = (*place.node).children[place.rank];
+	__bRes res = {NULL, 0};
+	int steps = 0;
+	while ((*actual).children[0]){
+		steps++;
+		actual =(*actual).children[(*actual).count];
+		if (!(*actual).empty)
+			res.rank = steps;
+	}
+	res.node = actual;
+	return res;
 }
 
 __bRes __bMinMax(__bRes place){
-    __bNode* actual = (*place.node).children[place.rank + 1];
-    __bRes res = {NULL, 0};
-    int steps = 0;
-    while ((*actual).children[0]){
-        steps++;
-        actual =(*actual).children[0];
-        if (!(*actual).empty)
-            res.rank = steps;
-    }
-    res.node = actual;
-    return res;
+	__bNode* actual = (*place.node).children[place.rank + 1];
+	__bRes res = {NULL, 0};
+	int steps = 0;
+	while ((*actual).children[0]){
+		steps++;
+		actual =(*actual).children[0];
+		if (!(*actual).empty)
+			res.rank = steps;
+	}
+	res.node = actual;
+	return res;
 }
 
 static __bRes __bClosest(__bRes place){
-    __bRes minMax = __bMinMax(place);
-    __bRes maxMin = __bMaxMin(place);
-    if (minMax.rank < maxMin.rank){
-        maxMin.rank = (*maxMin.node).count - 1;
-        return maxMin;
-    }
-    minMax.rank = 0;
-    return minMax;
+	__bRes minMax = __bMinMax(place);
+	__bRes maxMin = __bMaxMin(place);
+	if (minMax.rank < maxMin.rank){
+		maxMin.rank = (*maxMin.node).count - 1;
+		return maxMin;
+	}
+	minMax.rank = 0;
+	return minMax;
 }
 
 
@@ -135,27 +167,28 @@ static __bRes __bDeleteNode(__bRes place){
 
 
 __bRes __bFind(__bTree* tree, int value){
-    __bRes res = {NULL, -1};
-    __bNode actual = (*tree).root;
-    __bNode potential = actual;
-    bool found = false;
-    while (!found && potential){
-        actual = potential;
-        res.rank = __bDichoFind(value, (*actual).dividors, (*actual).count);
-        found = (*actual).dividors[index] == value;
-        bool past = (*actual).dividors[index] < value;
-        potential = (*actual).children[index + past];
-    }
-    res.node = actual;
-    return res;
+	__bRes res = {NULL, -1};
+	__bNode actual = (*tree).root;
+	__bNode potential = actual;
+	bool found = false;
+	while (!found && potential){
+		actual = potential;
+		int index = __bDichoFind(value, (*actual).dividors, (*actual).count);
+		found = (*actual).dividors[index] == value;
+		bool past = (*actual).dividors[index] < value;
+		potential = (*actual).children[index + past];
+		res.rank += found * (1 + index);
+	}
+	res.node = actual;
+	return res;
 }
 
 void __bDelete(__bTree* tree, int value){
 	const __bDeleteFunc func[2] = {&__bDeleteLeaf, &__bDeleteNode};
 
 	__bRes place = __bFind(tree, value);
-    if (place.rank == -1)
-        return;
+	if (place.rank == -1)
+		return;
 	(*func[(bool) (*place.node).children[0]])(place);
 	(*(*tree).root).empty = false;
 }
