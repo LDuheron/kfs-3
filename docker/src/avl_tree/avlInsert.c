4b99aff8856968fccf7448dddb39746a861ad0b4
Adrien THIERRY <52541958+A-Thierry@users.noreply.github.com>
2025-06-13 02:13:29 +0200

    Update avlInsert.c

--- docker/src/avl_tree/avlInsert.c
+++ docker/src/avl_tree/avlInsert.c
@@ -13,7 +13,25 @@
 #include "__avlTree.h"
 
 void __avlBalance(__avlTree* tree, __avlNode* node, ){
+const char values[2] = {-1; 1};
+__avNode* parent;
+bool elder;
+do{
+parent = (*node).parent;
+elder = (*node).elder
+char newParentBf = (*parent).balanceFactor + values[elder];
+bool unbalanced = (newParentBf == 2) || (newParentBf == -2);
+if (unbalanced)
+node = (*func[elder][newParentBf == -2])(node);
+else{
+node = parent;
+(*node).balanceFactor= newParentBf;
 }
+}while(parent)
+}
+
+
+
 void __avlInsert(__avlTree* tree, __avlData data){
 	__avlNode* current = (*tree).root;
 	__avlNode*	newNode = __avlNewNode(data);
