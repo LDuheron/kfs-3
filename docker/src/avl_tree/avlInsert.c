f65e0aa23b70ea700ffa5f537a63f7e686ace7a2
Adrien THIERRY <athierry@f2r8s12.clusters.42paris.fr>
2025-06-23 21:47:26 +0200

    pas avance

--- docker/src/avl_tree/avlInsert.c
+++ docker/src/avl_tree/avlInsert.c
@@ -6,28 +6,48 @@
 /*   By: athierry <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 17:58:59 by athierry          #+#    #+#             */
-/*   Updated: 2025/06/12 20:27:20 by athierry         ###   ########.fr       */
+/*   Updated: 2025/06/23 21:47:06 by athierry         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "__avlTree.h"
 
-void __avlBalance(__avlTree* tree, __avlNode* node, ){
-const char values[2] = {-1; 1};
-__avNode* parent;
-bool elder;
-do{
-parent = (*node).parent;
-elder = (*node).elder
-char newParentBf = (*parent).balanceFactor + values[elder];
-bool unbalanced = (newParentBf == 2) || (newParentBf == -2);
-if (unbalanced)
-node = (*func[elder][newParentBf == -2])(node);
-else{
-node = parent;
-(*node).balanceFactor= newParentBf;
+void rotate(__avlTree* tree, __avlNode* node){
+	int elder = (*node).elder;
+	__avlNode* parent = (*node).parent;
+	__avlNode* exchanged = (*node).children[!elder];
+	(*node).parent = (*parent).parent;
+	(*node).elder = (*parent).elder;
+	if ((*node).parent)
+		(*(*node).parent).children[(*parent).elder] = node;
+	(*parent).children[elder] = exchanged;
+	(*parent).elder = !elder;
+	(*parent).parent = node;
+	
+	(*exchanged).parent = parent;
+	(*exchanged).elder = elder;
+
+	(*node).children[!elder] = parent;
 }
-}while(parent)
+
+
+
+void __avlBalance(__avlTree* tree, __avlNode* node, ){
+	const char values[2] = {-1; 1};
+	__avNode* parent;
+	bool elder;
+	do{
+		parent = (*node).parent;
+		elder = (*node).elder;
+		char parentNewBf = (*parent).balanceFactor + values[elder];
+		bool unbalanced = (parentNewBf == 2) || (parentNewBf == -2);
+		if (unbalanced)
+			node = (*func[elder][parentNewBf == -2])(node);
+		else{
+			node = parent;
+			(*node).balanceFactor= parentNewBf;
+		}
+	}while(parent)
 }
 
 
